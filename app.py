a4da6d1d6435819b348bf8b367f06be2f491d097
Pushkar Singh <truepushkar@gmail.com>
2026-05-18 11:40:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -415,4 +415,4 @@ def api_submit():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0",port=5000, threaded=True)
+    app.run(debug=False, host="0.0.0.0",port=8000, threaded=True)
