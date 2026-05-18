bb948aeb8c9a306b611bccdc761f8dcf6023a1b0
Pushkar <truepushkar@gmail.com>
2026-05-18 11:44:12 +0530

    refactor: remove unused index.py file from API directory

--- app.py
+++ app.py
@@ -414,5 +414,5 @@ def api_submit():
                     headers={"Cache-Control": "no-cache", "X-Accel-Buffering": "no"})
 
 
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0",port=8000, threaded=True)
+#if __name__ == "__main__":
+#    app.run(debug=False, host="0.0.0.0",port=8000, threaded=True)
